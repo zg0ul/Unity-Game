ae0886e5b4f2c8ac470f12293f27a6c8ad0802dd
Mohammad Zgoul <moeazgoul@gmail.com>
2024-02-04 23:12:30 +0300

    Added object pick up and place functionality
    Added different types of container counters each summons a different object like bread, cabbage, tomato.. etc
    Added the functionality for the player to take an object from the container and place it on the clear counter

--- Assets/Scripts/ClearCounter.cs
+++ Assets/Scripts/ClearCounter.cs
@@ -9,6 +9,31 @@ public class ClearCounter : BaseCounter
 
     public override void Interact(Player player)
     {
-        
+        if (!HasKitchenObject())
+        {
+            // There is no KitchenObject here
+            if (player.HasKitchenObject())
+            {
+                // Player is carrying something
+                player.GetKitchenObject().SetKitchenObjectParent(this);
+            }
+            else
+            {
+                // Player not carrying anything.. do nothing
+            }
+        }
+        else
+        {
+            // There is a KitchenObject here
+            if (player.HasKitchenObject())
+            {
+                // Player is carrying something
+            }
+            else
+            {
+                // Player not carrying anything.. do nothing
+                GetKitchenObject().SetKitchenObjectParent(player);
+            }
+        }
     }
 }
