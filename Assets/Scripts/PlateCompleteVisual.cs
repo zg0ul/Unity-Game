01b6d3956d5bc08e212b0eef2c2dbcfaf0a1b561
Mohammad Zgoul <moeazgoul@gmail.com>
2024-02-09 21:51:50 +0300

    Added the ability to place objects on a plate
    Implemented two ways to place objects on a plate, either by holding the plate and interacting with a counter that has one of the objects that is allowed to be placed on a plate, or by holding one of those objects and interacting with a counter that has a plate on top.
    
    Added clear visual to the added objects on the plate.

---
+++ Assets/Scripts/PlateCompleteVisual.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlateCompleteVisual : MonoBehaviour
+{
+    [Serializable]
+    public struct KitchenObjectSO_GameObject
+    {
+        public KitchenObjectSO kitchenObjectSO;
+        public GameObject gameObject;
+    }
+
+    [SerializeField] private PlateKitchenObject plateKitchenObject;
+    [SerializeField] private List<KitchenObjectSO_GameObject> kitchenObjectSOGameObjectList;
+
+    private void Start()
+    {
+        plateKitchenObject.OnIngredientAdded += PlateKitchenObject_OnIngredientAdded;
+
+        // To initialize all game objects to be hidden instead of doing it in the editor manually
+        foreach (KitchenObjectSO_GameObject kitchenObjectSOGameObject in kitchenObjectSOGameObjectList)
+        {
+            kitchenObjectSOGameObject.gameObject.SetActive(false);
+        }
+    }
+
+    private void PlateKitchenObject_OnIngredientAdded(object sender, PlateKitchenObject.OnIngredientAddedEventArgs e)
+    {
+        foreach (KitchenObjectSO_GameObject kitchenObjectSOGameObject in kitchenObjectSOGameObjectList)
+        {
+            if (kitchenObjectSOGameObject.kitchenObjectSO == e.kitchenObjectSO)
+            {
+                kitchenObjectSOGameObject.gameObject.SetActive(true);
+            }
+        }
+        // e.kitchenObjectSO
+    }
+}
